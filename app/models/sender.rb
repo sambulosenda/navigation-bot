00ccef3d9036862e4e21aa860545f8c7232f72a2
kenzan8000 <kenzan8000@gmail.com>
2016-04-18 17:29:50 -0700

    fix uniquness

--- app/models/sender.rb
+++ app/models/sender.rb
@@ -1,7 +1,7 @@
 class Sender < ActiveRecord::Base
   has_many :steps, dependent: :destroy
   validates_presence_of :facebook_id, :navigation_status
-  validates_uniqueness_of :facebook_id
+  validates :facebook_id, uniqueness: true
 
   def self.recreate(facebook_id)
     sender = Sender.find_by_facebook_id facebook_id
